1d1d99734ffc6630d9642ea106b851987d3747bb
Samuel <samuel.gonzalez3@alu.uclm.es>
2022-01-29 11:12:08 +0100

    #unity pickup ammo from magPouch

---
+++ Stages Challenge/Assets/_Project/Scripts/MagPouchAmmo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MagPouchAmmo : MonoBehaviour
+{
+    [SerializeField] private GameObject _ammoPrefab;
+    
+    private GameObject _currentInstance;
+    private XRSocketInteractor _socket;
+
+    void Start()
+    {
+        _socket = GetComponent<XRSocketInteractor>();
+        _currentInstance = Instantiate(_ammoPrefab, _socket.attachTransform);
+        _socket.startingSelectedInteractable = _currentInstance.GetComponent<XRBaseInteractable>();
+    }
+
+    void Update()
+    {
+        if(!_socket.hasSelection)
+        {
+            _currentInstance.transform.SetParent(null);
+            _currentInstance = Instantiate(_ammoPrefab, _socket.attachTransform);
+        }
+    }
+}
